089030beab07803fdb21ed0f9a03c3ba0f1ee22c
Sinut W <s_w.piepie@outlook.com.au>
2022-06-30 20:22:57 +0700

    feat: update functionality

--- Article Preview Component/Article Preview Component/ViewController.swift
+++ Article Preview Component/Article Preview Component/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var profileStack: UIStackView!
+	@IBOutlet weak var shareStack: UIStackView!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+		shareStack.isHidden = true
 	}
 
-
+	@IBAction func pressedShareButton(_ sender: UIButton) {
+		
+		// swap displaying between profile and share
+		if profileStack.isHidden {
+			profileStack.isHidden = false
+			shareStack.isHidden = true
+		} else {
+			profileStack.isHidden = true
+			shareStack.isHidden = false
+		}
+		
+	}
+	
 }
 
