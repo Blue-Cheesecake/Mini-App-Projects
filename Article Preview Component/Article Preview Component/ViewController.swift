b1a4a5f3e0fc9a78e8d641e931faf53826bfea9e
Sinut W <s_w.piepie@outlook.com.au>
2022-06-30 15:04:45 +0700

    feat: update UI

---
+++ Article Preview Component/Article Preview Component/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Article Preview Component
+//
+//  Created by Cheese & Cupcake on 29/6/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
