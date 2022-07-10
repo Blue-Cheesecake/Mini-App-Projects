0b9ea204d51fee6d663dc25ae636a9907d8aa3b3
Sinut W <s_w.piepie@outlook.com.au>
2022-07-10 20:43:35 +0700

    feat: new functionality

--- Clima-iOS13/Clima/Controller/WeatherViewController.swift
+++ Clima-iOS13/Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,45 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
-    
-    override func viewDidLoad() {
+	@IBOutlet var searchTextField: UITextField!
+	
+	override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+		searchTextField.delegate = self
+		
     }
 
-
+	@IBAction func searchPressed(_ sender: UIButton) {
+		searchTextField.text = ""
+		searchTextField.placeholder = "Search"
+	}
+	
+	// The functions that we customize (override) for text field after we delegate searchTextField
+	func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+		if textField.text == "" {
+			textField.placeholder = "Type smthing"
+			return false
+		} else {
+			return true
+		}
+	}
+	
+	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+		print(searchTextField.text!)
+		searchTextField.endEditing(true)
+		return true
+	}
+	
+	func textFieldDidEndEditing(_ textField: UITextField) {
+		searchTextField.text = ""
+		searchTextField.placeholder = "Search"
+	}
+	
 }
 
