32b4c0981ebef3aca8340c7117ce9f7aebcf5376
Sinut W <s_w.piepie@outlook.com.au>
2022-07-13 01:26:07 +0700

    feat: add ui view

---
+++ Express Dictionary iOS/Express Dictionary iOS/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Express Dictionary iOS
+//
+//  Created by Cheese & Cupcake on 13/7/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+	
+}
+
---
+++ Express Dictionary iOS/README.md
@@ -0,0 +1,8 @@
+#  Express Dictionary
+
+An compact dictionary on your go. Available on iOS. This project would improve be about using API and designing delegate pattern.
+
+The user should insert text, when press search, it should show the definition and part of speech of given text.
+
+
+
