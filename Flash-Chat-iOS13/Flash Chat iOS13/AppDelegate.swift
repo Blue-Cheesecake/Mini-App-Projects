f46eeb1ec7adc933cc61f2220d72524dd386601d
Sinut W <s_w.piepie@outlook.com.au>
2022-07-18 16:03:04 +0700

    feat: auto move textField

--- Flash-Chat-iOS13/Flash Chat iOS13/AppDelegate.swift
+++ Flash-Chat-iOS13/Flash Chat iOS13/AppDelegate.swift
@@ -8,15 +8,17 @@
 
 import UIKit
 import FirebaseCore
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+	var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
 		FirebaseApp.configure()
+		IQKeyboardManager.shared.enable = true
         return true
     }
 
