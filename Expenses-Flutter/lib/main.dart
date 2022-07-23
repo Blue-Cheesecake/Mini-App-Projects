a44d1e8fa08f7adc5966f7b10b1eb9e56f1e255b
Sinut W <s_w.piepie@outlook.com.au>
2022-07-23 20:58:50 +0700

    feat: prototype

--- Expenses-Flutter/lib/main.dart
+++ Expenses-Flutter/lib/main.dart
@@ -1,21 +1,84 @@
 import 'package:flutter/material.dart';
 
+import './transaction.dart';
+
 void main() => runApp(const Main());
 
-class Main extends StatefulWidget {
+class Main extends StatelessWidget {
   const Main({Key? key}) : super(key: key);
 
   @override
-  State<Main> createState() => _MainState();
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Homepage(),
+    );
+  }
 }
 
-class _MainState extends State<Main> {
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  List<Transaction> transactions = [
+    Transaction("1", "Nike Shoes 69", 39.99, DateTime.now()),
+    Transaction("2", "Puma cal", 23.00, DateTime.now()),
+    Transaction("3", "Puma Ones", 109, DateTime.now()),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(),
-        body: Text("Hello World"),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Expenses"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Card(
+            child: Container(
+                padding: const EdgeInsets.all(10),
+                width: double.infinity,
+                child: const Text("Chart")),
+          ),
+          Column(
+            children: transactions.map((e) {
+              return Card(
+                child: Row(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.all(15),
+                      padding: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.purple, width: 2)),
+                      child: Text(
+                        e.price.toString(),
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold, color: Colors.purple),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          e.title,
+                          style: const TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        Text(e.date.toString(),
+                            style: const TextStyle(
+                                fontSize: 14, color: Colors.grey)),
+                      ],
+                    )
+                  ],
+                ),
+              );
+            }).toList(),
+          )
+        ],
       ),
     );
   }
---
+++ Expenses-Flutter/lib/transaction.dart
@@ -0,0 +1,8 @@
+class Transaction {
+  final String id;
+  final String title;
+  final double price;
+  final DateTime date;
+
+  Transaction(this.id, this.title, this.price, this.date);
+}
