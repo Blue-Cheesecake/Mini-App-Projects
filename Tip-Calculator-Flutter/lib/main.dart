cc12a096acf1c0d1833926d866274dbb9f9fbd14
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2022-08-14 20:35:33 +0700

    feat: assets

--- Tip-Calculator-Flutter/lib/main.dart
+++ Tip-Calculator-Flutter/lib/main.dart
@@ -16,7 +16,10 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Home(),
+      theme: ThemeData(
+        fontFamily: "SpaceMono",
+      ),
+      home: const Home(),
     );
   }
 }
--- Tip-Calculator-Flutter/lib/views/home.dart
+++ Tip-Calculator-Flutter/lib/views/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tip_calculator/utils/constants.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,8 +9,42 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  Widget _buildBillCard(BuildContext context) {
+    return LayoutBuilder(builder: (context, constraints) {
+      return Card(
+        color: ThemeColor.white,
+        child: Container(
+          width: double.infinity,
+          child: Column(
+            children: [],
+          ),
+        ),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: ThemeColor.lightGrayishCyan,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const Center(
+                child: Text(
+                  "SPLI\nTTER",
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                      letterSpacing: 10),
+                ),
+              ),
+              _buildBillCard(context),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
