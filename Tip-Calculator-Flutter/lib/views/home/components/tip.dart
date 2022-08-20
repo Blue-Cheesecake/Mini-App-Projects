1e0ad42731d883f502f01dbe0b5625cc8d0282f0
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2022-08-21 02:24:50 +0700

    feat: tip UI

--- Tip-Calculator-Flutter/lib/views/home/components/tip.dart
+++ Tip-Calculator-Flutter/lib/views/home/components/tip.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:tip_calculator/utils/constants.dart';
 
-class Tip extends StatefulWidget {
-  const Tip({Key? key}) : super(key: key);
+class Tip extends StatelessWidget {
+  final int tipValue;
+  final bool selected;
 
-  @override
-  State<Tip> createState() => _TipState();
-}
+  Tip({Key? key, required this.tipValue, required this.selected})
+      : super(key: key);
 
-class _TipState extends State<Tip> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(8)),
+        color: selected ? ThemeColor.strongCyan : ThemeColor.veryDarkCyan,
+      ),
+      width: 125,
+      height: 40,
+      child: Center(
+        child: Text(
+          "$tipValue%",
+          style: TextStyle(
+            fontSize: 23,
+            fontWeight: FontWeight.w700,
+            color: selected ? ThemeColor.veryDarkCyan : Colors.white,
+          ),
+        ),
+      ),
+    );
   }
 }
