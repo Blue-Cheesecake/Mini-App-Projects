178560c626999c62e471f078dca26ff346842e4e
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2022-11-25 00:49:29 +0700

    feat: starter

---
+++ DOB-Calculate-Android-5/app/src/main/java/com/sinut/dob_calculate_android_5/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sinut.dob_calculate_android_5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
