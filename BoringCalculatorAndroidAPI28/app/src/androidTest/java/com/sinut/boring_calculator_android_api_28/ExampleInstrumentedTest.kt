04f145b7ee8ceea1f54c6dab4979838a5be4840e
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2022-11-26 13:04:38 +0700

    feat: starter

---
+++ BoringCalculatorAndroidAPI28/app/src/androidTest/java/com/sinut/boring_calculator_android_api_28/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.sinut.boring_calculator_android_api_28
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.sinut.boring_calculator_android_api_28", appContext.packageName)
+    }
+}
---
+++ BoringCalculatorAndroidAPI28/app/src/main/java/com/sinut/boring_calculator_android_api_28/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sinut.boring_calculator_android_api_28
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
