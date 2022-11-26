0edad806940d1b2ec23a982d0804bfefb6c83297
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2022-11-26 16:34:31 +0700

    feat: del butt + result

--- BoringCalculatorAndroidAPI28/app/src/main/java/com/sinut/boring_calculator_android_api_28/MainActivity.kt
+++ BoringCalculatorAndroidAPI28/app/src/main/java/com/sinut/boring_calculator_android_api_28/MainActivity.kt
@@ -5,16 +5,21 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.isDigitsOnly
 
 class MainActivity : AppCompatActivity() {
 
     private var disText: TextView? = null
+    private var startNewString = false
 
     @SuppressLint("SetTextI18n")
     /**
      * @param n one character going to display
      */
     private fun displayText(n: String) {
+        if (!startNewString) {
+            clearText()
+        }
         if (n.length != 1) {
             throw Error("Can add only one text at a time")
         }
@@ -23,11 +28,94 @@ class MainActivity : AppCompatActivity() {
 
     private fun clearText() {
         disText?.text = ""
+        startNewString = true
+    }
+
+    private fun removeOneText(): Unit {
+        disText?.let {
+            if (disText!!.text.isEmpty()) {
+                return
+            }
+            val t = disText?.text!!
+            disText?.text = disText?.text!!.substring(0, t.lastIndex)
+        }
+    }
+
+    private fun isValidArray(arr: ArrayList<String>): Boolean {
+        // Not starting with operator
+        if (!arr[0].isDigitsOnly()) {
+            return false
+        }
+        // Operators should not be contiguous.
+        for (i in 0 until arr.size - 1) {
+            if (!arr[i].isDigitsOnly() && !arr[i + 1].isDigitsOnly()) {
+                return false
+            }
+        }
+
+        return true
     }
 
     private fun computeResult(): Unit {
-        TODO("Validate display text")
-        TODO("Compute the result")
+        if (disText == null) {
+            return
+        }
+
+        // Separated operators and numbers to ArrayList
+        val arrayList = ArrayList<String>()
+        val text = disText!!.text
+        var curr = ""
+        for (i in text.indices) {
+            if (text[i].isDigit()) {
+                curr += text[i]
+            }
+            if (i == text.lastIndex && text[i].isDigit()) {
+                arrayList.add(curr)
+            }
+            if (!text[i].isDigit()) {
+                if (curr != "") {
+                    arrayList.add(curr)
+                }
+                arrayList.add(text[i].toString())
+                curr = ""
+            }
+
+        }
+
+        // Valid the input
+        if (!isValidArray(arrayList)) {
+            disText!!.text = "Invalid Input"
+            startNewString = false
+            return
+        }
+
+        // Start Computing
+        var result = arrayList[0].toDouble()
+        for (i in 1 until arrayList.size - 1 step 2) {
+            val operator = arrayList[i]
+            val nextNum = arrayList[i + 1].toDouble()
+            if (operator == "+") {
+                result += nextNum
+            }
+            if (operator == "-") {
+                result -= nextNum
+            }
+            if (operator == "*") {
+                result *= nextNum
+            }
+            if (operator == "/") {
+                if (nextNum == 0.0) {
+                    disText!!.text = "Infinity"
+                    startNewString = false
+                    return
+                }
+                result /= nextNum
+            }
+        }
+
+
+        disText!!.text = result.toString()
+        startNewString = false
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,6 +137,7 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.omin).setOnClickListener { displayText("-") }
         findViewById<Button>(R.id.op).setOnClickListener { displayText("+") }
         findViewById<Button>(R.id.ocompute).setOnClickListener { computeResult() }
+        findViewById<Button>(R.id.delete).setOnClickListener { removeOneText() }
         findViewById<Button>(R.id.oclear).setOnClickListener { clearText() }
     }
 }
