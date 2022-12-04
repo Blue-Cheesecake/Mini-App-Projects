e035a6cabc2b4270df8e6e0e472f5d2dced3fe68
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2022-12-04 19:27:32 +0700

    rev

--- Interactive-Pricing-Component-Flutter/android/app/src/main/kotlin/com/example/interactive_pricing_component/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.interactive_pricing_component
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- product_preview_card_flutter/android/app/src/main/kotlin/com/sinut/product_preview_card_flutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.sinut.product_preview_card_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
