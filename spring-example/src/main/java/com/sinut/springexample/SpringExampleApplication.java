ab1d6e01a14a50eb9b71ef791ab10d713e7b79d9
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2023-05-01 12:35:08 +0700

    chore: init project 🫀

---
+++ spring-example/src/main/java/com/sinut/springexample/SpringExampleApplication.java
@@ -0,0 +1,13 @@
+package com.sinut.springexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExampleApplication.class, args);
+	}
+
+}
---
+++ spring-example/src/test/java/com/sinut/springexample/SpringExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinut.springexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
