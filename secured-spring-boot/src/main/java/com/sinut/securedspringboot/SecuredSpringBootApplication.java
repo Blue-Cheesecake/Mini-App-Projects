61e35a17fdc27540e9512c42f17d9a843881096f
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2023-05-01 14:32:50 +0700

    chore: init project 🫀

---
+++ secured-spring-boot/src/main/java/com/sinut/securedspringboot/SecuredSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.sinut.securedspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuredSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuredSpringBootApplication.class, args);
+	}
+
+}
---
+++ secured-spring-boot/src/test/java/com/sinut/securedspringboot/SecuredSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinut.securedspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuredSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
