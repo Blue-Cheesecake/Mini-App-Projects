a31c2fbf2623b2e9c41f70832629d0cc884fbc8d
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2023-05-30 18:03:07 +0700

    feat: filtering structure

--- Job-Listings/flutter_client/lib/features/jobs/presentation/views/job_view.dart
+++ Job-Listings/flutter_client/lib/features/jobs/presentation/views/job_view.dart
@@ -5,29 +5,47 @@ import 'package:flutter_client/features/jobs/presentation/widgets/job_filter_wid
 import 'package:flutter_client/features/jobs/presentation/widgets/job_item_widget.dart';
 import 'package:provider/provider.dart';
 
-class JobView extends StatelessWidget {
+class JobView extends StatefulWidget {
   const JobView({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final JobService jobService = Provider.of<JobService>(
+  State<JobView> createState() => _JobViewState();
+}
+
+class _JobViewState extends State<JobView> {
+  bool _isFiltering = false;
+  late final JobService _jobService;
+
+  @override
+  void initState() {
+    super.initState();
+    _jobService = Provider.of<JobService>(
       context,
       listen: false,
     );
+  }
 
+  void reverseIsFilteringState() {
+    setState(() {
+      _isFiltering = !_isFiltering;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 30),
         child: Column(
           children: [
-            const JobFilterWidget(),
-            const SizedBox(height: 35),
+            _isFiltering ? const JobFilterWidget() : const SizedBox.shrink(),
+            _isFiltering ? const SizedBox(height: 35) : const SizedBox.shrink(),
             FutureBuilder(
-              future: jobService.getJobs(),
+              future: _jobService.getJobs(),
               builder: (context, snapshot) {
                 if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const CircularProgressIndicator();
+                  return const Center(child: CircularProgressIndicator());
                 }
 
                 List<JobModel>? data = snapshot.data;
@@ -43,7 +61,10 @@ class JobView extends StatelessWidget {
                     shrinkWrap: true,
                     itemCount: data.length,
                     itemBuilder: (context, index) {
-                      return JobItemWidget(jobModel: data[index]);
+                      return Padding(
+                        padding: const EdgeInsets.only(bottom: 25),
+                        child: JobItemWidget(jobModel: data[index]),
+                      );
                     },
                   ),
                 );
