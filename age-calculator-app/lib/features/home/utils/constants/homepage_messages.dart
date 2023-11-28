e52697f026885f37621427337eeef419466fc2b2
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2023-11-28 22:39:29 +0700

    feat: MNAP-0 constant messages

--- age-calculator-app/lib/features/home/utils/constants/homepage_messages.dart
+++ age-calculator-app/lib/features/home/utils/constants/homepage_messages.dart
@@ -1,3 +1,29 @@
 class HomepageMessages {
   const HomepageMessages._();
+
+  /// Text Fields Header
+  ///
+  static const day = 'DAY';
+  static const month = 'MONTH';
+  static const year = 'YEAR';
+
+  /// Result Texts Header
+  ///
+  static const days = 'days';
+  static const months = 'months';
+  static const years = 'years';
+  static const empty = '--';
+
+  /// Placeholders
+  ///
+  static const dayPlaceholder = 'DD';
+  static const monthPlaceholder = 'MM';
+  static const yearPlaceholder = 'YYYY';
+
+  /// Error Texts
+  ///
+  static const invalidDayErrorText = 'Must be a valid day';
+  static const invalidMonthErrorText = 'Must be a valid month';
+  static const invalidYearErrorText = 'Must be in the past';
+  static const requiredErrorText = 'This field is required';
 }
