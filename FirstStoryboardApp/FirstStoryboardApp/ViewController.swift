f826279c070104a92d38521d9a6083782022fcd3
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2024-07-13 23:46:59 +0700

    feat: toggle image

--- FirstStoryboardApp/FirstStoryboardApp/ViewController.swift
+++ FirstStoryboardApp/FirstStoryboardApp/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    var isBuilding = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    ///   This function will executed only if the button is clicked
+    ///
+    ///   - Parameter sender: any sender
+    @IBAction func onChangeImage(_ sender: Any) {
+        print("Toogling image")
+        
+        if isBuilding {
+            imageView.image = UIImage(named: "open_graph_logo")
+        } else {
+            imageView.image = UIImage(named: "3-TIS_Building")
+        }
+        
+        isBuilding = !isBuilding
+    }
 }
 
