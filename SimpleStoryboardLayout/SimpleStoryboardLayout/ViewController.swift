f03123894a4fb9cc473256233e0b5301a7af47e4
SINUT WATTANARPORN <s_w.piepie@outlook.com.au>
2024-07-16 17:50:24 +0700

    feat: constraints storyboard

---
+++ SimpleStoryboardLayout/SimpleStoryboardLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleStoryboardLayout
+//
+//  Created by Cheese Macaron on 16/7/2567 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
+
